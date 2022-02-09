422b780eb81fee300b0f3e53438422a4fc6cb570
shravankt79 <shravankt79@gmail.com>
2022-02-09 01:26:05 -0600

    wrok will progress from tomorrow

--- TC_Edit_customer_006.java
+++ TC_Edit_customer_006.java
@@ -1,3 +1,4 @@
 // This Test case is created for CR in GitHub on 02/02/2022
 // going to work on the CR from tomorrow 02/03/2022
 // work is in progress today 02/08/2022
+//work will be continue from 03/08/2022
